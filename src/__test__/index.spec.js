fde62ceeb03342444877c2476ab11abefd26b3b9
Jonathan Grimes <jsg2021@gmail.com>
2018-02-19 14:09:05 -0600

    add a test to cover a recent failure

--- src/__test__/index.spec.js
+++ src/__test__/index.spec.js
@@ -28,6 +28,42 @@ describe('html-reactifier tests', () => {
 		expect(result).toEqual(['div', {}, ['div', {}, 'test']]);
 	});
 
+	it('Handles html entities in attributes', () => {
+		const f = getRenderer('A<br><br><p>B</p><br> <br><p>C</p><p></p><p><span style="font-size: 11.0pt; font-family: &quot;Calibri&quot;,&quot;sans-serif&quot;;">D</span>   </p><br><br>E<br><br>F<br><br>G');
+		const React = {createElement () { return [...arguments]; }};
+
+		spyOn(React, 'createElement').and.callThrough();
+
+		const result = f(React);
+
+		const args = ['div', {},
+			'A',
+			['br', {}, undefined],
+			['br', {}, undefined],
+			['p', {}, 'B'],
+			['br', {}, undefined],
+			' ',
+			['br', {}, undefined],
+			['p', {}, 'C'],
+			['p', {}, undefined],
+			['p', {}, ['span', {}, 'D'], '   '],
+			['br', {}, undefined],
+			['br', {}, undefined],
+			'E',
+			['br', {}, undefined],
+			['br', {}, undefined],
+			'F',
+			['br', {}, undefined],
+			['br', {}, undefined],
+			'G'
+		];
+
+		expect(React.createElement).toHaveBeenCalledWith(...args);
+
+		//Typically this should be the return value of React.createElement... our test just verifies that it was called.
+		expect(result).toEqual(args);
+	});
+
 
 	it('Identify Widgets/Components/Template-parts', () => {
 		const self = {
