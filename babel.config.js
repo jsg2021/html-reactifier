103eada4e30a515c49cc30f08e5c0592ddcaf571
Jonathan Grimes <jsg2021@gmail.com>
2018-10-02 14:05:36 -0500

    update babel

---
+++ babel.config.js
@@ -0,0 +1,24 @@
+'strict mode';
+
+module.exports = function (api) {
+	if (api && api.cache) {
+		api.cache(() => process.env.NODE_ENV);
+	}
+
+	return {
+		sourceType: 'unambiguous',
+		compact: false,
+		"presets": [
+			['@babel/preset-env', {
+				shippedProposals: true,
+				targets: {
+					browsers: [
+						"last 2 versions",
+						"not dead"
+					]
+				}
+			}]
+		],
+		"sourceMaps": "both"
+	};
+}
